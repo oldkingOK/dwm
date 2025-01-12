d7d401a9f776d9a3915284224ebfd5949a92a09d
oldkingOK <oldkingok@qq.com>
2025-01-12 15:42:27 +0800

    feat: add backlight hotkey

--- config.h
+++ config.h
@@ -60,12 +60,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slockcmd[] = { "slock", NULL };
+static const char *backlightupcmd[] = { "xbacklight", "-inc", "20", NULL };
+static const char *backlightdowncmd[] = { "xbacklight", "-dec", "20", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
+	{ 0     ,                       XF86XK_MonBrightnessUp, spawn,          {.v = backlightupcmd } },
+	{ 0     ,                       XF86XK_MonBrightnessDown, spawn,          {.v = backlightdowncmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
